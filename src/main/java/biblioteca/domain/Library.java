59631e7e410f1f41de06ce259134ba0d36ec466b
Kanisorn S <firstziiz.k@gmail.com>
2019-06-18 11:42:37 +0700

    [1.9] refactor naming of message

--- src/main/java/biblioteca/domain/Library.java
+++ src/main/java/biblioteca/domain/Library.java
@@ -3,6 +3,10 @@ package biblioteca.domain;
 import java.util.ArrayList;
 
 public class Library {
+    private static final String CHECKOUT_SUCCESS_MESSAGE = "Thank you! Enjoy your book!";
+    private static final String CHECKOUT_NOT_AVAILABLE_MESSAGE = "The book is not available.";
+    private static final String CHECKOUT_BOOK_NOT_APPEAR_MESSAGE = "The book is not appear in the shelf.";
+
     ArrayList<Book> books = new ArrayList<Book>() {{
         add(new Book("Clean Code", "2008", "Uncle Bob"));
         add(new Book("The Refactoring", "1999", "Martin Fowler"));
@@ -16,12 +20,12 @@ public class Library {
         try {
             if (this.books.get(index).isAvailable()) {
                 this.books.get(index).setAvailable(false);
-                return "Thank you! Enjoy your book!";
+                return CHECKOUT_SUCCESS_MESSAGE;
             } else {
-                return "The book is not available.";
+                return CHECKOUT_NOT_AVAILABLE_MESSAGE;
             }
         } catch (IndexOutOfBoundsException exception) {
-            return "The book is not appear in the shelf.";
+            return CHECKOUT_BOOK_NOT_APPEAR_MESSAGE;
         }
     }
 
