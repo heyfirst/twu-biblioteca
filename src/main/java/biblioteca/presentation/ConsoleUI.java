51ee535acc1d6f1936425c089c7cdbd10e92a10f
Kanisorn S <firstziiz.k@gmail.com>
2019-06-18 10:45:10 +0700

    [1.4] add menu in ui

--- src/main/java/biblioteca/presentation/ConsoleUI.java
+++ src/main/java/biblioteca/presentation/ConsoleUI.java
@@ -4,15 +4,23 @@ import biblioteca.domain.Book;
 import biblioteca.domain.Library;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ConsoleUI {
     public static void run() {
         Library library = new Library();
-        System.out.println("Welcome to Biblioteca.");
+        Scanner sc = new Scanner(System.in);
+        int select = 0;
 
-        ArrayList<Book> books = library.getBooks();
-        for(int i = 1; i <= books.size(); i++) {
-            System.out.println(i + ". " + books.get(i - 1));
+        System.out.println("Welcome to Biblioteca.");
+        System.out.println("------- Menus -------");
+        System.out.println("[1] List of Books");
+        select = sc.nextInt();
+        if (select == 1) {
+            ArrayList<Book> books = library.getBooks();
+            for(int i = 1; i <= books.size(); i++) {
+                System.out.println(i + ". " + books.get(i - 1));
+            }
         }
     }
 }
--- src/test/java/biblioteca/presentation/ConsoleUITest.java
+++ src/test/java/biblioteca/presentation/ConsoleUITest.java
@@ -3,6 +3,7 @@ package biblioteca.presentation;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,15 +11,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ConsoleUITest {
     @Rule
     public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    @Rule
+    public final TextFromStandardInputStream systemInMock  = TextFromStandardInputStream.emptyStandardInputStream();
 
     @Test
     public void shouldBePrintWelcomeMessageWhenStartProgram() {
+        systemInMock.provideLines("1");
         ConsoleUI.run();
         assertThat(systemOutRule.getLog(), containsString("Welcome to Biblioteca."));
     }
 
     @Test
+    public void shouldBeShowListOfMenuWhenStartProgram() {
+        systemInMock.provideLines("1");
+        ConsoleUI.run();
+        assertThat(systemOutRule.getLog(), containsString("------- Menus -------"));
+    }
+
+    @Test
     public void shouldBeShowListOfBooksAfterWelcomeMessage() {
+        systemInMock.provideLines("1");
         ConsoleUI.run();
         assertThat(systemOutRule.getLog(), containsString("1. Clean Code (2008) Uncle Bob\n2. The Refactoring (1999) Martin Fowler"));
     }
